5370b86b71bcab5f0c7f9ce141be256ba76a2507
Ben Seward <ben@bens-mbp.home>
2018-03-04 14:28:04 +0000

    refactored some stuff, added the app.layout to tasks page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
     <div id="app">
@@ -31,7 +33,7 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav ml-auto pull-right">
                         <!-- Authentication Links -->
                         @guest
                             <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,81 +1,74 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>My to-do List App</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </head>
-  <body>
-    <div class="container">
-      <div class="col-md-offset-1 col-md-10">
-        <div class="row">
-          <h1>To Do List</h1>
+@extends('layouts.app')
+@section('content')
+  <div class="container">
+    <div class="col-md-offset-1 col-md-10">
+      <div class="row">
+        <h1>To Do List</h1>
+        <h3>Hello, {{ Auth::user()->name }}</h3>
+      </div>
+      @if(Session::has('success'))
+        <div class="alert alert-success">
+          <strong>Success:</strong> {{ Session::get('success') }}
         </div>
-        @if(Session::has('success'))
-          <div class="alert alert-success">
-            <strong>Success:</strong> {{ Session::get('success') }}
-          </div>
-        @endif
+      @endif
 
 
-        @if(count($errors) > 0 )
-          <div class="alert alert-danger">
-            <strong>Error:</strong>
-            <ul>
-              @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-              @endforeach
-            </ul>
-          </div>
-        @endif
-        <div class="row">
-
-          <form class="" action="{{ route('tasks.store') }}" method="POST">
-            {{ csrf_field() }}
-            <div class="col-md-9">
-              <input type="text" name="newTaskName" class="form-control">
-            </div>
-            <div class="col-md-3">
-              <input type="submit" class="btn btn-primary btn-block" value="Add Task">
-            </div>
-          </form>
-
-        </div>
-        @if(count($storedTasks) > 0)
-        <table class="table">
-          <thead>
-            <th>Task Number</th>
-            <th>Name</th>
-            <th>Status</th>
-            <th>Edit</th>
-            <th>Delete</th>
-          </thead>
-
-          <tbody>
-            @foreach($storedTasks as $storedTask)
-            <tr>
-              <th>{{ $storedTask->id }}</th>
-              <td>{{ $storedTask->name }}</td>
-              <td>{{ $storedTask->status }}</td>
-              <td><a class="btn btn-default" href="{{ route('tasks.edit', ['tasks' => $storedTask->id]) }}">Edit</a></td>
-              <td>
-                <form action="{{ route('tasks.destroy', ['tasks' => $storedTask->id]) }}" method="post">
-                  {{ csrf_field() }}
-                  <input type="hidden" name="_method" value="DELETE">
-                  <input class="btn btn-danger" type="submit" name="" value="Delete">
-                </form>
-              </td>
-            </tr>
+      @if(count($errors) > 0 )
+        <div class="alert alert-danger">
+          <strong>Error:</strong>
+          <ul>
+            @foreach($errors->all() as $error)
+              <li>{{ $error }}</li>
             @endforeach
-          </tbody>
-        </table>
-        @endif
-
-        <div class="row text-center">
-          {{ $storedTasks->links() }}
+          </ul>
         </div>
+      @endif
+      <div class="row">
+
+        <form class="col-md-12" action="{{ route('tasks.store') }}" method="POST">
+          {{ csrf_field() }}
+          <div class="col-md-9">
+            <input type="text" name="newTaskName" class="form-control">
+          </div>
+          <div class="col-md-3">
+            <input type="submit" class="btn btn-primary btn-block" value="Add Task">
+          </div>
+        </form>
+
+      </div>
+      @if(count($storedTasks) > 0)
+      <table class="table">
+        <thead>
+          <th>Task Number</th>
+          <th>Name</th>
+          <th>Status</th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </thead>
+
+        <tbody>
+          @foreach($storedTasks as $storedTask)
+          <tr>
+            <th>{{ $storedTask->id }}</th>
+            <td>{{ $storedTask->name }}</td>
+            <td>{{ $storedTask->status }}</td>
+            <td><a class="btn btn-default" href="{{ route('tasks.edit', ['tasks' => $storedTask->id]) }}">Edit</a></td>
+            <td>
+              <form action="{{ route('tasks.destroy', ['tasks' => $storedTask->id]) }}" method="post">
+                {{ csrf_field() }}
+                <input type="hidden" name="_method" value="DELETE">
+                <input class="btn btn-danger" type="submit" name="" value="Delete">
+              </form>
+            </td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+      @endif
+
+      <div class="row text-center">
+        {{ $storedTasks->links() }}
       </div>
     </div>
-  </body>
-</html>
+  </div>
+@endsection
